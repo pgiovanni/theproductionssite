0a5753e7cc043b790427be79f1dca5c9781dfd3c
Paul <pgioavnni1234@gmail.com>
2020-05-26 21:54:35 -0400

    python stuff

--- serverside/name-email.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $userName = htmlspecialchars($_POST ['userName']);
-    $userEmail = htmlspecialchars($_POST ['userEmail']);
-
-    echo $userName, '', $userEmail;
-
-?>
---
+++ serverside/name-email.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    return render_template('../serverside/signup.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return render_template('greeting.html', userName=request.form['userName'], userEmail=request.form['to'])
+
+if __name__ == "__main__":
+    app.run()
