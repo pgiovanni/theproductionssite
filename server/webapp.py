4551f22de37fd09f4373904976eade9ae00913e2
Paul <pgioavnni1234@gmail.com>
2020-06-06 00:07:29 -0400

    saved

--- server/webapp.py
+++ server/webapp.py
@@ -15,3 +15,23 @@ app = application
 @app.route('/')
 def index():
     return Response(response=render_template('index.html'), status=200)
+
+@app.route('/child_templates/<html_name>')
+def html_reciever(html_name):
+    return Response(response=render_template(html_name), status=200)
+
+@app.template_global()
+def path_to_styles(filename):
+    return '/music-site/styles/' + filename
+
+@app.template_global()
+def path_to_scripts(filename):
+    return '/music-site/scripts/' + filename
+
+@app.template_global()
+def path_to_images(filename):
+    return '/music-site/images/' + filename
+
+@app.template_global()
+def path_to_templates(filename):
+    return '/webapp/child_templates/' + filename
