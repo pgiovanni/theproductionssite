b67c6c876b43971d6fbadfbe374e25879e3119aa
Paul <pgioavnni1234@gmail.com>
2020-06-05 23:21:15 -0400

    webapp

--- server/webapp.py
+++ server/webapp.py
@@ -2,10 +2,9 @@ import sys
 from pprint import pprint
 sys.path.append('/usr/local/lib/python3.8/site-packages')
 
-import ptvsd
-ptvsd.enable_attach(('localhost', 9999),redirect_output=True) 
-ptvsd.wait_for_attach()
-
+#import ptvsd
+#ptvsd.enable_attach(('localhost', 9999),redirect_output=True) 
+#ptvsd.wait_for_attach()
 
 from flask import Flask, Response, render_template, request
 
@@ -14,7 +13,5 @@ app = application
 
 
 @app.route('/')
-    return Response(response="hello world I\'m a dumb web app", status=200)
-
-@app.route('/music')
-    render_template 
+def index():
+    return Response(response=render_template('index.html'), status=200)
