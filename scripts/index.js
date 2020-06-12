e04c049e492931fd416ae2bca8a782b57fe04fec
Paul <pgioavnni1234@gmail.com>
2020-06-12 10:52:51 -0400

    working on js to make template sticky

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+
+
+function scroll_locker () {
+
+    const tour=document.getElementById(tourContainer);
+    const sml=document.getElementById(smlContainer); 
+    const music=document.getElementById(musicContainer);
+    const shop=document.getElementById(shopContainer);
+    const signup=document.getElementById(signupContainer);
+    const video=document.getElementById(videoContainer);
+
+
+
+
+
+    tour.addEventListener('scroll', );
+    sml.addEventListener('scroll', );
+    music.addEventListener('scroll', );
+    shop.addEventListener('scroll', );
+    signup.addEventListener('scroll', );
+    video.addEventListener('scroll', );
+
+}
+
