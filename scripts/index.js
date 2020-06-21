bb819ecedc8d3bf8017b939f35123825ac54416b
Paul <pgioavnni1234@gmail.com>
2020-06-21 11:36:57 -0400

    working with media queries

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,30 @@
 let signup_button=document.getElementById('signupContainer').onclick = function signup_scroll() {
 
   let form_container=document.getElementById('email_form');
+  let svs=document.getElementById('video');
+  let svs1=document.getElementById('shop');
+  let svs2=document.getElementById('signupButton');
  
-  form_container.style.display="block";
-  form_container.style.transform="translateY(-100px)";
+  if(form_container.style.opacity==0) {
 
+  form_container.style.opacity=1;
+  form_container.style.transform="translateY(-100%)";
+  svs.style.transform="translateY(-130px)";
+  svs1.style.transform="translateY(-130px)";
+  svs2.style.transform="translateY(-130px)";
+
+  }
+
+  else  {
+
+    form_container.style.opacity=0;
+    form_container.style.transform="translateY(100%)";
+    svs.style.transform="translateY(.5px)";
+    svs1.style.transform="translateY(.5px)";
+    svs2.style.transform="translateY(1px)";
   
   
-}
+
+  }
+  
+ }
---
+++ scripts/video.js
@@ -0,0 +1,26 @@
+let signup_button=document.getElementById('signupContainer').onclick = function signup_scroll() {
+
+    let form_container=document.getElementById('email_form');
+    let svs1=document.getElementById('shop');
+    let svs2=document.getElementById('signupButton');
+   
+    if(form_container.style.opacity==0) {
+  
+    form_container.style.opacity=1;
+    form_container.style.transform="translateY(-100%)";
+    svs1.style.transform="translateY(-130px)";
+    svs2.style.transform="translateY(-130px)";
+  
+    }
+  
+    else  {
+  
+      form_container.style.opacity=0;
+      form_container.style.transform="translateY(100%)";
+      svs1.style.transform="translateY(.5px)";
+      svs2.style.transform="translateY(1px)";
+      
+    }
+    
+   }
+  
